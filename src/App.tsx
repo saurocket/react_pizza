ba2594ae15288d751b4ca07fdbfc7540eaeda30f
Yauheni Luzakou <luzakou@yandex.ru>
2021-05-26 15:07:38 +0200

    fixed redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
 import {Header} from "./components/Header/Header";
 
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {ProductsList} from "./components/ProductsList";
 import {Basket} from "./components/Bascket/Basket";
-import {BrowserRouter, Route} from 'react-router-dom'
+import {BrowserRouter, Redirect, Route} from 'react-router-dom'
 import {MyOrder} from "./components/MyOrder/MyOrder";
+import {useSelector} from "react-redux";
+import {AppStateType} from "./redux/strore";
+
+
+
 
 function App() {
+    const isEmptyfromState = useSelector((state:AppStateType) => state.basketPage.isEmpty)
+    const [isEmpty, setEmpty] = useState(false)
+
+    useEffect(() => {
+        setEmpty(isEmptyfromState)
+        },[isEmptyfromState])
     return (
         <BrowserRouter>
             <div className="wrapper">
                 <Header/>
-                <Route path='/' exact>
-                    <ProductsList/>
-                </Route>
-                <Route path='/basket'>
-                    <Basket/>
-                </Route>
-                <Route path='/order'>
-                    <MyOrder/>
-                </Route>
+                {!isEmpty && <Redirect to="/" />}
+                       <Route path='/' exact>
+                           <ProductsList/>
+                       </Route>
+                    <Route path='/basket'>
+                        <Basket/>
+                    </Route>
+                    <Route path='/order'>
+                        <MyOrder/>
+                    </Route>
             </div>
         </BrowserRouter>
     );
